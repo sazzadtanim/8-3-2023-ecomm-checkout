1e5c2c048f6775d0c7917caef125c0f45c69feb9
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-04 00:43:56 +0600

    added new product images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,28 @@
 import Headline from "@/components/Headline";
 import Input, { InputProps } from "@/components/Input";
 import ProductCard, { ProductCardProps } from "@/components/ProductCard";
+import anc from "@png/anc.png";
+import camera from "@png/camera.jpg";
+import speaker from "@png/speaker.jpg";
 
 const productCards: ProductCardProps[] = [
   {
     model: "TOASB-2S",
     title: "portable stereo speaker",
     price: "230.49",
+    image: speaker,
   },
   {
     model: "JC00-SB",
     title: "i-Type Instant Camera",
     price: "630.20",
+    image: camera,
+  },
+  {
+    model: "JH151-SH",
+    title: "Positive Vibration ANC ",
+    price: "330.00",
+    image: anc,
   },
 ];
 
@@ -49,8 +60,8 @@ function LeftSection() {
       <div className="schedule_delivery">
         <Headline title="schedule delivery" />
         <div className="form bg-white p-10 flex flex-col gap-5">
-          <Input label="Dates" placeholder="17 apr,23 - 19 Apr,23" />
-          <Input label="note" placeholder="type your note" type={"text"} />
+          <Input label="Dates" placeholder="17 Apr, 23  -  19 Apr, 23" />
+          <Input label="note" placeholder="Type your note" type={"text"} />
         </div>
       </div>
 
@@ -72,31 +83,37 @@ function LeftSection() {
 
 function RightSection() {
   return (
-    <div className="right_section max-w-2xl flex-1 h-full">
-      <Headline title="order summary" />
-      <div className="product_summary_cart p-10 bg-white">
+    <div className="right_section max-w-2xl flex-1 grid">
+      <div className="absolute">
+        {" "}
+        <Headline title="order summary" />
+      </div>
+      <div className="product_summary_cart p-10 bg-white mt-12 flex flex-col justify-between">
         <div className="flex flex-col gap-10 mb-10">
           {productCards.map((product, index) => (
             <ProductCard key={index} {...product} />
           ))}
         </div>
-        <div className="calculation">
-          <div>
-            <span>subtotal</span>
-            <span>1250.32</span>
+        <div className="calculation grid">
+          <div className="border-y ring-gray-200 py-5 flex flex-col gap-5">
+            <div className="flex capitalize justify-between">
+              <span className="font-light text-gray-500">subtotal</span>
+              <span>$1250.32</span>
+            </div>
+            <div className="flex capitalize justify-between">
+              <span className="font-light text-gray-500">shipping</span>
+              <span> -- </span>
+            </div>
           </div>
 
-          <div>
-            <span>shipping</span>
-            <span> -- </span>
+          <div className="flex capitalize justify-between py-5">
+            <span className="font-light text-gray-500">total(usd)</span>
+            <span>$S7250</span>
           </div>
 
-          <div>
-            <span>total(usd)</span>
-            <span>7250</span>
-          </div>
-
-          <button>confirm order</button>
+          <button className="bg-[#22331D] text-white capitalize py-2 w-full mx-auto">
+            confirm order
+          </button>
         </div>
       </div>
     </div>
