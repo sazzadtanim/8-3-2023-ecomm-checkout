386539158a3de56b9193d881996252534f5f4341
Sazzad Hossain <tanimsazzad@gmail.com>
2023-08-06 22:43:52 +0600

    productcard small size fixed

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -18,23 +18,25 @@ function ProductCard(props: ProductCardProps) {
         <Image
           alt=''
           src={image ?? ''}
-          className='h-16 w-16 object-contain'
+          className='sm:h-16 sm:w-16 object-contain w-14 h-14'
           priority
         />
         <div className='grid font-sans leading-6'>
           <span className='text-xs font-bold text-gray-800 sm:text-base sm:font-semibold'>
             {title}
           </span>
-          <span className=' text-sm text-gray-400'>{model}</span>
-          <span className='font-sans font-semibold'>${price}</span>
+          <span className='text-xs text-gray-400 sm:text-sm '>{model}</span>
+          <span className='font-sans text-sm font-semibold sm:text-base'>
+            ${price}
+          </span>
         </div>
       </div>
 
-      <div className='flex items-center justify-center gap-4 px-4 py-2 ring-1 ring-gray-200'>
+      <div className='flex items-center justify-center gap-4 px-3 py-[1px] ring-1 ring-gray-200 sm:px-4 sm:py-2'>
         <button className='text-2xl' onClick={() => setCounter(counter - 1)}>
           -
         </button>
-        <span className='font-sans font-medium'>
+        <span className='font-sans font-medium text-sm sm:text-base'>
           {counter < 10 && counter > 0 ? `0${counter}` : counter}
         </span>
         <button className='text-2xl' onClick={() => setCounter(counter + 1)}>
